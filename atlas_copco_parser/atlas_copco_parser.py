0f8a03037aab65d65ef9ca63549a55c0c6885345
ErnstFinkAG <it@fink-holzbau.ch>
2025-09-25 14:21:57 +0200

    updated

--- atlas_copco_parser/atlas_copco_parser.py
+++ atlas_copco_parser/atlas_copco_parser.py
@@ -16,7 +16,7 @@ import paho.mqtt.client as mqtt
 
 OPTIONS_PATH = "/data/options.json"
 SELF_PATH = __file__
-VERSION = "0.8.1-entityid-fix-2025-09-04"
+VERSION = "0.9.1"
 
 # ------------------------- PowerShell QUESTION (exact) ------------------------
 QUESTION_HEX_GS15VP13 = (
